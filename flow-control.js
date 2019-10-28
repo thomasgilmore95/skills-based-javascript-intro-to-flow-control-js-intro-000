9dcb5c397d6147567b12000ed2a1767efc0f0a8b
Thomas Gilmore <tomtitans@aol.com>
2019-10-28 01:34:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,7 +11,11 @@ return "You are a teenager!";
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+  return "You are a teenager!";
+  } else if (age <= 12) {
+  return "You are a kid";
+  } else {return "You are a grownup";}
 }
 
 function ternaryTeenager(age) {
